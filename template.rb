9cb56ddd9cd09943ddbf2aafb9f8551ae8e7db63
James Pearson <mail@jamespearson.email>
2024-02-16 10:19:11 +0000

    ✨ Added RSpec setup

--- template.rb
+++ template.rb
@@ -1,3 +1,23 @@
+def rspec_setup
+    generate "rspec:install"
+
+    # Append requires to spec/rails_helper.rb
+    append_to_file 'spec/rails_helper.rb', after: "require 'rspec/rails'\n" do
+      <<~RUBY
+  
+        # Added by template
+        require 'capybara/rspec'
+        require 'faker'
+        require 'support/database_cleaner' # Uncomment this line if you have a support file for DatabaseCleaner
+      RUBY
+    end
+
+    dirname = File.basename(Dir.getwd)
+    puts "dirname: #{dirname}"
+    puts "__dir__: #{__dir__}"
+    
+    directory "#{__dir__}/rspec/support", "rspec/support"
+end
 remove_file "public/index.html"
 remove_file "public/favicon.ico"
 remove_file "public/images/rails.png"
@@ -24,4 +44,7 @@ end
 
 gem_group :development do
     gem 'annotate'
+end
+after_bundle do
+    rspec_setup
 end
