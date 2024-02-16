be4d6fb41d0614b4b4120adbc446d80c339cec70
James Pearson <mail@jamespearson.email>
2024-02-16 10:44:29 +0000

    💄 Reorder of functions

--- template.rb
+++ template.rb
@@ -1,45 +1,4 @@
-def rspec_setup
-    generate "rspec:install"
 
-    # Append requires to spec/rails_helper.rb
-    append_to_file 'spec/rails_helper.rb', after: "require 'rspec/rails'\n" do
-      <<~RUBY
-  
-        # Added by template
-        require 'capybara/rspec'
-        require 'faker'
-        require 'support/database_cleaner' # Uncomment this line if you have a support file for DatabaseCleaner
-      RUBY
-    end
-
-    dirname = File.basename(Dir.getwd)
-    puts "dirname: #{dirname}"
-    puts "__dir__: #{__dir__}"
-    
-    directory "#{__dir__}/rspec/support", "rspec/support"
-end
-
-def pundit_setup
-    generate "pundit:install"
-    directory "#{__dir__}/app/policies","app/policies", force: true
-
-    # Add pundit to the application controller
-    inject_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do
-        <<~RUBY
-        include Pundit::Authorization
-
-        rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-        after_action :verify_policy_scoped
-      
-        def user_not_authorized
-            flash[:alert] = "You are not authorized to perform this action."
-            redirect_to(request.referrer || root_path)
-        end
-        RUBY
-    end
-        
-end
 
 def devise_setup
     generate "devise:install"
@@ -73,6 +32,51 @@ def devise_setup
         RUBY
     end
 end
+
+
+def pundit_setup
+    generate "pundit:install"
+    directory "#{__dir__}/app/policies","app/policies", force: true
+
+    # Add pundit to the application controller
+    inject_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::Base\n" do
+        <<~RUBY
+        include Pundit::Authorization
+
+        rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+        after_action :verify_policy_scoped
+      
+        def user_not_authorized
+            flash[:alert] = "You are not authorized to perform this action."
+            redirect_to(request.referrer || root_path)
+        end
+        RUBY
+    end
+        
+end
+
+def rspec_setup
+    generate "rspec:install"
+
+    # Append requires to spec/rails_helper.rb
+    append_to_file 'spec/rails_helper.rb', after: "require 'rspec/rails'\n" do
+      <<~RUBY
+  
+        # Added by template
+        require 'capybara/rspec'
+        require 'faker'
+        require 'support/database_cleaner' # Uncomment this line if you have a support file for DatabaseCleaner
+      RUBY
+    end
+
+    dirname = File.basename(Dir.getwd)
+    puts "dirname: #{dirname}"
+    puts "__dir__: #{__dir__}"
+    
+    directory "#{__dir__}/rspec/support", "rspec/support"
+end
+
 
 remove_file "public/index.html"
 remove_file "public/favicon.ico"
@@ -108,6 +112,8 @@ after_bundle do
     rspec_setup
     pundit_setup
     devise_setup
+    
+    database_setup
 end
 
 
